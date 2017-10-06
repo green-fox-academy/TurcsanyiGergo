00df5a54e937f55b25a6129a5ed96371e86dbf9c
Turcsanyi Gergely <tgergo821206@gmail.com>
2017-10-06 11:29:59 +0200

    Delete the unnecessary files and completed the saved files

---
+++ week-01/day-3/Exercise1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int a = 3;
+	// make it bigger by 10
+	a = a + 10;
+	printf("The current value of the a is  %d.\n", a);
+	return 0;
+}
---
+++ week-01/day-3/Exercise2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int b = 100;
+	// make it smaller by 7
+	b = b - 7;
+	printf("The new value of b is = %d", b);
+
+	return 0;
+}
