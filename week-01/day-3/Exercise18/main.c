02ca2d7889b47b624f68f2bb000b990734304d03
Turcsanyi Gergely <tgergo821206@gmail.com>
2017-10-07 21:42:40 +0200

    Exercise 12 to the right place

--- week-01/day-3/Exercise18/main.c
+++ week-01/day-3/Exercise18/main.c
@@ -6,7 +6,7 @@ int main() {
 	// if number can't be represented with 8bits
 	// print 'longer than 8bits' otherwise print 'ok'
         if (number < 256){
-        printf("%d is OK, because it is smaller than 256!", number);
+        printf("%d is OK, because it is smaller than 256!\n", number);
 	}
 	return 0;
 }
