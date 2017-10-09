7fcca546fa754b299548eea9e6e2f6c056c373a6
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-09 15:54:26 +0200

    Printer

--- week-02/Day-1/Printer/main.c
+++ week-02/Day-1/Printer/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "printer.h"
+// Create a function called `printer`
+// which prints the input parameters
+// can have multiple number of arguments
+// store your variables in a .h file
+int sum(int num1, int num2, int num3, int num4);
+void printer(int num1, int num2, int num3, int num4);
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+
+    printer(a, b, c, d);
+    printf("%d", sum(a, b, c, d));
     return 0;
 }
+
+void printer(int num1, int num2, int num3, int num4)
+{
+    printf("%d %d %d %d\n", num1, num2, num3, num4);
+}
+
+int sum(int num1, int num2, int num3, int num4)
+{
+    return num1 + num2 + num3 + num4;
+}
+
--- week-02/Day-1/Printer/printer.h
+++ week-02/Day-1/Printer/printer.h
@@ -1,1 +1,4 @@
-
+int a = 5;
+int b = 15;
+int c = 25;
+int d = 50;
