27c26cf3abb5ca0d03e38703ffcb2a8a80226117
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-11 21:17:21 +0200

    Doubling

---
+++ week-02/Day-1/Doubling/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "doubling.h"
+// create a function called `doubling` that doubles it's input parameter
+// again the parameters value should be stored in a .h file
+
+int doubling (number);
+
+int main()
+{
+    int doubling(number);
+    printf("%d", doubling (number));
+
+    return 0;
+}
+
+    int doubling(number){
+    int result = number * 2;
+
+return result;
+
+}
