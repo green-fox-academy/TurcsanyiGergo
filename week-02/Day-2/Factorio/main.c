bdaf13f5a2cf6391c70e5ca879b2fd635752e3cc
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-11 14:40:42 +0200

    Factorio

--- week-02/Day-2/Factorio/main.c
+++ week-02/Day-2/Factorio/main.c
@@ -6,19 +6,32 @@
 // again the parameters value should be stored in a .h file
 //
 int factorio();
-int fact = 2;
-int result = 0;
+
 int main()
 {
-    factorio();
+
+int fact = 0;
+    printf("Please give me a number for the calciulation:\n");
+    scanf("%d", &fact);
+
+    printf("The result is: %d.", factorio(fact));
     return 0;
 }
 
 
 
 int factorio(fact){
-    for(int i = 1; i <= fact ; i++){
-       printf("%d\n" , result = i*i);
+    if(fact == 0) {
+        return 1;
     }
-return ;
+    int result = fact;
+    for(int i = 1 ; i < fact; i++){
+        //for(int g = fact - 1; g >= 1; g--){
+        result = result * i;
+    }
+
+
+    return result;
 }
+
+
