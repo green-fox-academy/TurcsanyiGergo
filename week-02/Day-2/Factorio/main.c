854018da22bc3fe1b2c88ef2b6bd6f3cc9b14720
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-11 13:00:53 +0200

    Factorio start

---
+++ week-02/Day-2/Factorio/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// create a function called `factorio`
+// that returns it's input's factorial with and without recursion
+// again the parameters value should be stored in a .h file
+//
+int factorio();
+int fact = 2;
+int result = 0;
+int main()
+{
+    factorio();
+    return 0;
+}
+
+
+
+int factorio(fact){
+    for(int i = 1; i <= fact ; i++){
+       printf("%d\n" , result = i*i);
+    }
+return ;
+}
