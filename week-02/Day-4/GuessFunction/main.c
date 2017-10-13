836d9b2b47620a98bf2cf2fafd1aed4b8f5e618f
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-13 10:48:14 +0200

    Guess with fuction

---
+++ week-02/Day-4/GuessFunction/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int guess();
+
+int main() {
+
+   guess();
+
+
+   return 0;
+   }
+
+
+int guess(){
+
+    srand(time(NULL));
+    int count , user_number = 0, random_number = rand() % 100;
+
+
+    for (count = 0; count < 5; count++) {
+        printf("Please enter your number: \n>");
+        scanf("\n%d", &user_number);
+
+        if(user_number == random_number) {
+            printf("Yessss, you are the best!");
+            break;
+    }
+
+        if(user_number > random_number)
+            printf("Too big! You have %d lives left.\n\n", 4-count);
+        else
+            printf("Too small. You have %d lives left.\n\n", 4 - count);
+
+    }
+
+    if(count == 5)
+         printf("No luck at this time, please try again! \nThe number was: %d. \n", random_number);
+
+return 0;
+
+}
