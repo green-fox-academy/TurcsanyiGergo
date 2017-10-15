a0a5d189b74979da3a8db55205e0c2944726b46a
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-15 14:22:30 +0200

    Exercise11nicer

--- week-01/day-4/Exercise11/main.c
+++ week-01/day-4/Exercise11/main.c
@@ -14,20 +14,26 @@ int main() {
     for(i = 1; i <= 100; i++) {
 
     switch(i % 15){
-        case 0 : printf("FizzBuzz\n"); continue;
+        case 0 :
+            printf("FizzBuzz\n");
+            continue;
 
     }
 
     switch (i % 3){
-        case 0 : printf("Fizz\n"); continue;
+        case 0 :
+            printf("Fizz\n");
+            continue;
 
     }
     switch (i % 5){
-        case 0 : printf("Buzz\n"); continue;
+        case 0 :
+            printf("Buzz\n");
+            continue;
 
     }
 
-                printf("%d\n", i);
+            printf("%d\n", i);
     }
 
     return 0;
