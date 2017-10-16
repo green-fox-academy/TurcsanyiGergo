d0806c52f7bb210b60927e1a73059e2e0af9df25
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-16 23:02:25 +0200

    GuessFunction modified

--- week-02/Day-4/GuessFunction/main.c
+++ week-02/Day-4/GuessFunction/main.c
@@ -2,23 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-int guess();
+void user_message();
+int generate_random_number();
+void guess();
 
-int main() {
-
-   guess();
-
+int main()
+{
+    user_message();
+    generate_random_number();
+    guess();
 
    return 0;
-   }
+}
 
-
-int guess(){
-
+int generate_random_number()
+{
     srand(time(NULL));
+    int random_number = rand() % 101;
+    return random_number;
+}
+
+void guess()
+{
+
+    int random_number = generate_random_number();
+
     int counter = 0;
     int user_number = 0;
-    int random_number = rand() % 101;
+
 
 
     for (counter = 0; counter < 5; counter++) {
@@ -40,6 +51,10 @@ int guess(){
     if(counter == 5){
          printf("No luck at this time, please try again! \nThe number was: %d. \n", random_number);
     }
-return 0;
 
 }
+
+    void user_message()
+    {
+      printf("Dear User! This is the Guess my number computer game. Try to find the number the computer generated!\n\n");
+    }
