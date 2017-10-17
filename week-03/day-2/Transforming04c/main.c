0cd47b3a3a5eded260a61609e0933f09369bd464
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-17 13:34:26 +0200

    Transforming

---
+++ week-03/day-2/Transforming04c/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	float val;
+	char str[5];
+	strcpy(str, "3.14");
+
+	//TODO: print out the value of pi, first as a string, then a float value.
+   float val = float str;
+    printf("%f", str);
+	return(0);
+}
