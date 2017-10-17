ae1eead41b1d35e23dd2be683dfb1e7b6af13a1c
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-17 11:37:42 +0200

    TypeCasting02c

---
+++ week-03/day-2/TypeCasting02c/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	double x = 1.2;
+    //TODO: write a program wich adds up x and 1, and print out it rounded.
+    double result = x + 1;
+    printf("The rounded result is: %.f.\n", result);
+
+    return 0;
+}
