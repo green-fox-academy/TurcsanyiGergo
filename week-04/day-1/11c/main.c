77addafccce7e6c70183163ce8feec32d2eebc10
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-24 15:32:23 +0200

    10c-11c

--- week-04/day-1/11c/main.c
+++ week-04/day-1/11c/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void calc(int a, int b, int *p_sum, int *p_mul);
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	int x, y;
+	x = 5;
+	y = 8;
+
+	int sum, mul;
+    calc(x, y, &sum, &mul);
+	//TODO: write a void function which calculates the sum and multiplication of x and y according to the function prototype. Use 'output parameters'. Print out these values.
+    printf("Sum of a and b is: %d.\nMultiplication of a and b is: %d.\n", sum, mul);
+
+	return 0;
+}
+
+void calc(int a, int b, int *p_sum, int *p_mul) {
+
+    *p_sum = a + b;
+    *p_mul = a * b;
+
 }
