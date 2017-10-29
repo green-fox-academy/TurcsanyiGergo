623219fd867ad19d5059258509eea36733bf642b
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-30 00:30:47 +0100

    Arrays3c

--- week-02/Day-2/Arrays3c/main.c
+++ week-02/Day-2/Arrays3c/main.c
@@ -3,17 +3,13 @@
 int main() {
     int int_array[] = {1, 654, 987, 654, 31};
     float float_array[] = {1.2, 3.14, 1.41, 0.707, 1.73};
-
-	//TODO:
+    //TODO:
     // Print out the content of the arrays
-    for(int i = 0; i <=4; i++){
-                printf("%d : %d\n", i, int_array[i]);
-
+    for (int i = 0; i < sizeof (int_array)/sizeof (int_array[0]); i++) {
+        printf("%d\n", int_array[i]);
     }
-    printf("\n");
-
-    for(int g = 0; g <=4; g++){
-                printf("%d : %.3f\n", g, float_array[g]);
+    for (int i = 0; i < sizeof (float_array)/sizeof (float_array[0]); i++) {
+        printf("%.3f\n", float_array[i]);
     }
     return 0;
 }
