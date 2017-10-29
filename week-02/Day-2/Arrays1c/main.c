02002b4ee37d737800c43ad6d75ca468a06a8b64
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-30 00:24:57 +0100

    Arrays1cgood

--- week-02/Day-2/Arrays1c/main.c
+++ week-02/Day-2/Arrays1c/main.c
@@ -3,13 +3,10 @@
 int main() {
     char my_name_array[] = "My Name is Gergo";
 
-
     for(int i = 0; i < sizeof (my_name_array); i++) {
         printf("%c \n", my_name_array[i]);
     }
-
 	//TODO:
     // Print out the array character-by-character
-
     return 0;
 }
