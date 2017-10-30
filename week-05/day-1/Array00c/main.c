7f3866aa8f75e49e47869df4c27e2fc9130792ec
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-10-30 11:03:51 +0100

    Array00cworking

--- week-05/day-1/Array00c/main.c
+++ week-05/day-1/Array00c/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+void main()
 {
     int Gergo_array[5];
+    printf("The original order is:\n");
+    for(int i = 0; i < 5; i++){
+        Gergo_array[i] = rand() * 0.5;
 
-    for (int i = 0; i < 5; i++) {
-    Gergo_array[i] = rand() * 0.5;
         printf("%d | ", Gergo_array[i]);
     }
+        printf("\n");
 
-    for (int j = 4; j >= 0; j--) {
-        printf("%d | ", Gergo_array[j]);
-   }
+        printf("The reversed order is:\n");
 
-    return 0;
+    int temp = 0;
+    int i = 0;
+    int j = 0;
+
+    for(i = 0, j = 4; i <= j; i++, j--){
+        temp = Gergo_array[i];
+        Gergo_array[i] = Gergo_array[j];
+        Gergo_array[j] = temp;
+    }
+
+    for(i = 0; i < 5; i++){
+        printf("%d | ", Gergo_array[i]);
+    }
+        printf("\n");
 }
