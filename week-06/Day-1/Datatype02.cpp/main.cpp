5ffc2ceb95b6b63fb53ec6c6513e47084df72977
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-13 14:51:32 +0100

    5th

---
+++ week-06/Day-1/Datatype02.cpp/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
+int main()
+{
+
+string name = "Gergely Turcsanyi";
+int age = 34;
+double height = 1.82;
+
+
+cout << name << endl;
+cout << age << endl;
+cout << height << endl;
+
+    return 0;
+}
