7cb3ca0da6eb483d528e514fc7c53ae916b78739
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-13 13:43:11 +0100

    2nd

---
+++ week-06/Day-1/01cppInputOutput/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+
+int main()
+{
+    string name1 = "Zoli";
+    string name2 = "Adam";
+    string name3 = "Tapanyagszamitos Peti";
+
+    cout << "Hello, " << name1 << "!" << endl;
+    cout << "Hello, " << name2 << "!" << endl;
+    cout << "Hello, " << name3 << "!" << endl;
+
+    return 0;
+}
