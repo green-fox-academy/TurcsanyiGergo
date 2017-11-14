6877541307199ac7696d3ef1061da2014248ffc1
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-14 23:12:49 +0100

    Works

--- week-06/Day-2/08cpp/main.cpp
+++ week-06/Day-2/08cpp/main.cpp
@@ -44,6 +44,13 @@ float get_average(){
 
 int main() {
 
+    srand(time(NULL));
+
+    Student s;
+
+    s.add_grade();
+
+    cout << endl << "\nThe average grade is: " << s.get_average() << "." << endl;
     // Create a student Class
     // that has a method `add_grade`, that takes a grade from 1 to 5
     // an other method `get_average`, that returns the average of the
