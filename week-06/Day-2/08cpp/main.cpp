b780fc49f83e9a723b67ac817cdc81aa0b345bb2
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-14 22:46:19 +0100

    8thStart

---
+++ week-06/Day-2/08cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+
+class Student {
+
+    private:
+        int grade;
+        int size_v;
+        vector <int> myVector;
+
+    public:
+        //Student() {
+       // }
+
