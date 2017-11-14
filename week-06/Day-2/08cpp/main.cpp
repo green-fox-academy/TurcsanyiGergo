64a2fffd987b96c7f8026c01c623802c9f0ab01c
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-14 22:56:33 +0100

    1stFunction

--- week-06/Day-2/08cpp/main.cpp
+++ week-06/Day-2/08cpp/main.cpp
@@ -16,4 +16,15 @@ class Student {
     public:
         //Student() {
        // }
+int add_grade() {
+            size_v = 5;
+
+            for (unsigned int i = 0; i < size_v; ++i)
+                myVector.push_back(rand() % 5 + 1);
+                cout << "The grades are: ";
+            for (unsigned int i = 0; i < size_v; ++i)
+                cout << myVector[i] << " ";
+
+            return 0;
+        }
 
