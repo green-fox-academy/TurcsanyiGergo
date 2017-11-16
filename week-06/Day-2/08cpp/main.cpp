083226113b92581cac8c0d3d6661e22fe12a8ef8
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-16 14:45:13 +0100

    08CPP

--- week-06/Day-2/08cpp/main.cpp
+++ week-06/Day-2/08cpp/main.cpp
@@ -8,41 +8,38 @@ using namespace std;
 
 class Student {
 
-    private:
-        int grade;
-        int size_v;
-        vector <int> myVector;
-
-    public:
-        //Student() {
-       // }
-        ~Student() {
-    cout << endl << "This program is finished." << endl;
+private:
+    int grade;
+    int size_v;
+    vector <int> myVector;
+public:
+    //Student() {
+    // }
+    ~Student() {
+        cout << endl << "This program is finished." << endl;
     }
-int add_grade() {
-
-            size_v = 5;
-
-            for (unsigned int i = 0; i < size_v; ++i)
-                myVector.push_back(rand() % 5 + 1);
-                cout << "The grades are: ";
-            for (unsigned int i = 0; i < size_v; ++i)
-                cout << myVector[i] << " ";
-
-            return 0;
+void add_grade() {
+    size_v = 5;
+        for (unsigned int i = 0; i < size_v; ++i)
+            myVector.push_back(rand() % 5 + 1);
         }
+
+void printer(){
+    cout << "The grades are: ";
+    for (unsigned int i = 0; i < size_v; ++i)
+        cout << myVector[i] << " ";
+}
 
 float get_average(){
-            float sum;
-            float avg;
+    float sum;
+    float avg;
+    for (int i = 0; i < size_v; ++i){
+        sum += (float)myVector[i];
+    }
+    avg = sum / (float)size_v;
 
-            for (int i = 0; i < size_v; ++i){
-                sum += (float)myVector[i];
-            }
-            avg = sum / (float)size_v;
-
-            return avg;
-        }
+    return avg;
+}
 };
 
 int main() {
@@ -50,15 +47,13 @@ int main() {
     srand(time(NULL));
 
     Student s;
-
     s.add_grade();
-
+    s.printer();
     cout << endl << "\nThe average grade is: " << s.get_average() << "." << endl;
     // Create a student Class
     // that has a method `add_grade`, that takes a grade from 1 to 5
     // an other method `get_average`, that returns the average of the
     // grades
 
-  return 0;
+    return 0;
 }
-
