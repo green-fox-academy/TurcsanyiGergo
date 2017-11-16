ecb59e3856d0463257c945adc05c085ede952727
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-16 09:50:35 +0100

    Vectors02cpp

---
+++ week-06/Day-1/Vectors02cpp/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	//Create a vector of integers
+	//reserve place for 10 element, then fill those place with random numbers between 0 and 10
+    vector <int> int_vect(10);
+    //int_vect.resize(15);
+
+    srand(time(NULL));
+
+
+   for (unsigned int i = 0; i < int_vect.size(); ++i){
+        int_vect[i] = (rand() % 10);
+    }
+
+    for (unsigned int i = 0; i < int_vect.size(); ++i) {
+        cout << int_vect[i] << endl;
+    }
+
+  return 0;
+}
