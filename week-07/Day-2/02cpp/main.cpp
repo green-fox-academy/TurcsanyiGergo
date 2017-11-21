e6c7a6684af1bd8bb1bb950bd5f121818dad7ba7
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-21 11:48:32 +0100

    02cppNotReady

---
+++ week-07/Day-2/02cpp/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+// Create 3 functions: each should take respectively 2, 3 and 4 strings.
+// Each should return a string that contains all the input string concatenated.
+// (Combined into one in a linear order.)
+// Write code that showcases how this works and prints out the result of the function.
+
+string stringFunction(string a, string b);
+string stringFunction(string a, string b, string c);
+string stringFunction(string a, string b, string c, string d);
+
+
+int main() {
+
+    cout <<
+
+	return 0;
+}
+
+string stringFunction(string a, string b) {
+
+    return a + b;
+}
+
+string stringFunction(string a, string b, string c) {
+
+    return a + b + c;
+}
+
+string stringFunction(string a, string b, string c, string d) {
+
+    return a + b + c + d;
+}
