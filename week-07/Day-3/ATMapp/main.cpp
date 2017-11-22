ab9a2edb9ed887c0858173828afdd9cdaba2af99
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-22 11:32:30 +0100

    ATMapp3rd

--- week-07/Day-3/ATMapp/main.cpp
+++ week-07/Day-3/ATMapp/main.cpp
@@ -7,10 +7,9 @@ class UserData {
 private:
     string name;
     unsigned int PinCode;
-    unsigned int AdminPinCode;
     int money;
 public:
-    UserData (string name, unsigned int PinCode, unsigned int AdminPinCode, int money) : name(name), PinCode(PinCode), AdminPinCode(AdminPinCode), money(money){}
+    UserData (string name, unsigned int PinCode, int money) : name(name), PinCode(PinCode), money(money){}
 
 
 };
@@ -35,3 +34,15 @@ class Customer : public Users {
 class Administrator : public Users {
 
 };
+
+
+int main() {
+
+    UserData u1("Gergo", 3456, 3213000);
+    UserData u2("Tomi", 2459, 13456723);
+    UserData u3("GeoBazsi", 3981, 3812344);
+    UserData u4("Zoli", 4592, 6578972);
+    UserData u5("Adam", 7392, 22678987);
+
+    return 0;
+}
