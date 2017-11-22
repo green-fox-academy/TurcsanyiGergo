7b1c9ca0363298dc6180152525d62b8c13f92078
Gergely Turcsanyi <31779995+TurcsanyiGergo@users.noreply.github.com>
2017-11-22 13:00:00 +0100

    ATMapp4th

--- week-07/Day-3/ATMapp/main.cpp
+++ week-07/Day-3/ATMapp/main.cpp
@@ -10,17 +10,32 @@ private:
     int money;
 public:
     UserData (string name, unsigned int PinCode, int money) : name(name), PinCode(PinCode), money(money){}
-
+    int GetMoney() {
+    return money;
+    }
+    string GetName() {
+    return name;
+    }
 
 };
 
 class Users {
 private:
     vector<UserData> UserList;
+public:
     void addUser(UserData id) {
     UserList.push_back(id);
-
-
+    }
+    void PrintRichest() {
+    float max_money = 0;
+    unsigned int max_index = 0;
+    for(int i = 0; i < UserList.size(); i++) {
+        if (UserList.at(i).GetMoney() > max_money) {
+            max_money = UserList.at(i).GetMoney();
+            max_index = i;
+            }
+        }
+        cout << "The richest person is: " << UserList.at(max_index).GetName() << ".";
     }
 
 
@@ -43,6 +58,15 @@ int main() {
     UserData u3("GeoBazsi", 3981, 3812344);
     UserData u4("Zoli", 4592, 6578972);
     UserData u5("Adam", 7392, 22678987);
+
+    Users BankUsers;
+    BankUsers.addUser (u1);
+    BankUsers.addUser (u2);
+    BankUsers.addUser (u3);
+    BankUsers.addUser (u4);
+    BankUsers.addUser (u5);
+
+
 
     return 0;
 }
